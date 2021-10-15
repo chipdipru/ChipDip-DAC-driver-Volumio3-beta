d860efc0949109231402c8cd7bf41aaa9fbdac14
chipdip <43340836+chipdipru@users.noreply.github.com>
2021-10-15 11:11:00 +0300

    Update chipdip-dac-install.sh

--- chipdip-dac-install.sh
+++ chipdip-dac-install.sh
@@ -2,8 +2,8 @@
 
 echo "Installing ChipDip DACs..."
 
-sudo cp ChipDip-DAC-driver-Volumio3/snd-soc-chipdip-dac-5.4.ko /lib/modules/`uname -r`/kernel/sound/soc/bcm/snd-soc-chipdip-dac.ko
-sudo cp ChipDip-DAC-driver-Volumio3/chipdip-dac.dtbo /boot/overlays/chipdip-dac.dtbo
+sudo cp ChipDip-DAC-driver-Volumio3-beta/snd-soc-chipdip-dac-5.4.ko /lib/modules/`uname -r`/kernel/sound/soc/bcm/snd-soc-chipdip-dac.ko
+sudo cp ChipDip-DAC-driver-Volumio3-beta/chipdip-dac.dtbo /boot/overlays/chipdip-dac.dtbo
 sudo depmod
 
 sed -i '4 a {"id":"chipdip-master-dac","name":"ChipDip DAC","overlay":"chipdip-dac","alsanum":"2","mixer":"","modules":"","script":"","needsreboot":"yes"},' /volumio/app/plugins/system_controller/i2s_dacs/dacs.json
